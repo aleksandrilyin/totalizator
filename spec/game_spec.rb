060f939b359f7cb54266c5b8dde41de8c91a5997
Alexander Ilyin <alexander.ilyin11@gmail.com>
2020-05-12 23:58:57 +0300

    Create spec

---
+++ spec/game_spec.rb
@@ -0,0 +1,9 @@
+require 'game'
+
+describe Game do
+  it 'returns the score' do
+    expect(Game.score(0, 1, 0, 1)).to eq(1)
+    expect(Game.score(2, 1, 1, 0)).to eq(0)
+    expect(Game.score(0, 4, 2, 0)).to eq(-1)
+  end
+end
