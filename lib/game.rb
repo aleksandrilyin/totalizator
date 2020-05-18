8ab154481c241fd4e936682afb93917da4f61feb
Alexander Ilyin <alexander.ilyin11@gmail.com>
2020-05-18 23:59:11 +0300

    Create lib/game.rb

---
+++ lib/game.rb
@@ -0,0 +1,17 @@
+module Game
+  def score(real_score_team1, real_score_team2, estimated_score_team1, estimated_score_team2)
+    if real_score_team1 == estimated_score_team1 &&
+    real_score_team2 == estimated_score_team2
+      1
+    elsif real_score_team1 > real_score_team2 &&
+    estimated_score_team1 > estimated_score_team2 ||
+    real_score_team1 < real_score_team2 &&
+    estimated_score_team1 < estimated_score_team2
+      0
+    else
+      -1
+    end
+  end
+
+  module_function :score
+end
