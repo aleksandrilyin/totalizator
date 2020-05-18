0c3d72a4fbfb2962d1807f61c923ea6afcca2ba3
Alexander Ilyin <alexander.ilyin11@gmail.com>
2020-05-18 23:53:42 +0300

    Update spec to game_spec.rb

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,6 +4,7 @@ describe Game do
   it 'returns the score' do
     expect(Game.score(0, 1, 0, 1)).to eq(1)
     expect(Game.score(2, 1, 1, 0)).to eq(0)
+    expect(Game.score(1, 2, 0, 1)).to eq(0)
     expect(Game.score(0, 4, 2, 0)).to eq(-1)
   end
 end
