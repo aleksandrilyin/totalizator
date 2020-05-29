bc0b2624396a17a973a3454a75681358de2917d0
Alexander Ilyin <alexander.ilyin11@gmail.com>
2020-05-29 23:01:55 +0300

    Add input range

--- app.rb
+++ app.rb
@@ -3,10 +3,10 @@ require_relative 'lib/game'
 real_score_team1 = rand(10)
 real_score_team2 = rand(10)
 
-print 'Enter the estimated score of the game of team 1: '
+print 'Enter the estimated score of the game of team 1 (0-9): '
 estimated_score_team1 = gets.strip
 
-print 'Enter the estimated score of the game of team 2: '
+print 'Enter the estimated score of the game of team 2 (0-9): '
 estimated_score_team2 = gets.strip
 
 puts "\nThe estimated score of the game: #{estimated_score_team1}:#{estimated_score_team2}"
