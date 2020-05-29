afbab9dbc930a2082b0af9e78472c05619d00ca5
Alexander Ilyin <alexander.ilyin11@gmail.com>
2020-05-29 23:08:49 +0300

    Update spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,15 @@
 require 'game'
 
-describe Game do
-  it 'returns the score' do
-    expect(Game.score(0, 1, 0, 1)).to eq(1)
-    expect(Game.score(2, 1, 1, 0)).to eq(0)
-    expect(Game.score(1, 2, 0, 1)).to eq(0)
-    expect(Game.score(0, 4, 2, 0)).to eq(-1)
+RSpec.describe Game do
+  describe '#score' do
+    it 'returns the score' do
+      expect(Game.score(0, 1, 0, 1)).to eq(1)
+      expect(Game.score(0, 0, 0, 0)).to eq(1)
+      expect(Game.score(2, 1, 1, 0)).to eq(0)
+      expect(Game.score(1, 2, 0, 1)).to eq(0)
+      expect(Game.score(1, 1, 0, 0)).to eq(0)
+      expect(Game.score(0, 0, 1, 1)).to eq(0)
+      expect(Game.score(0, 4, 2, 0)).to eq(-1)
+    end
   end
 end
