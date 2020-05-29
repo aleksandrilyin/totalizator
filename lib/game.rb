b8e8e140d40f08e304bdc24cf3336730590b80eb
Alexander Ilyin <alexander.ilyin11@gmail.com>
2020-05-29 23:15:30 +0300

    Change operator

--- lib/game.rb
+++ lib/game.rb
@@ -3,10 +3,10 @@ module Game
     if real_score_team1 == estimated_score_team1 &&
     real_score_team2 == estimated_score_team2
       1
-    elsif real_score_team1 > real_score_team2 &&
-    estimated_score_team1 > estimated_score_team2 ||
-    real_score_team1 < real_score_team2 &&
-    estimated_score_team1 < estimated_score_team2
+    elsif real_score_team1 >= real_score_team2 &&
+    estimated_score_team1 >= estimated_score_team2 ||
+    real_score_team1 <= real_score_team2 &&
+    estimated_score_team1 <= estimated_score_team2
       0
     else
       -1
