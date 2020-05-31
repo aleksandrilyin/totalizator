19f5bf303da9c49138fef61308f87f44a8933a82
Alexander Ilyin <alexander.ilyin11@gmail.com>
2020-05-31 18:03:38 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,13 +4,13 @@ real_score_team1 = rand(10)
 real_score_team2 = rand(10)
 
 print 'Enter the estimated score of the game of team 1 (0-9): '
-estimated_score_team1 = gets.strip
+estimated_score_team1 = gets.strip.to_i
 
 print 'Enter the estimated score of the game of team 2 (0-9): '
-estimated_score_team2 = gets.strip
+estimated_score_team2 = gets.strip.to_i
 
-puts "\nThe estimated score of the game: #{estimated_score_team1}:#{estimated_score_team2}"
-puts "The real score of the game: #{real_score_team1}:#{real_score_team2}"
+puts "\nThe real score of the game: #{real_score_team1}:#{real_score_team2}"
+puts "The estimated score of the game: #{estimated_score_team1}:#{estimated_score_team2}"
 
 game = Game.score(real_score_team1, real_score_team2, estimated_score_team1, estimated_score_team2)
 puts "\nScore: #{game}"
