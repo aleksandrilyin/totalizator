3a4157f50cde90549eb858b1a5bfce4067b4977f
Alexander Ilyin <alexander.ilyin11@gmail.com>
2021-12-15 12:34:58 +0300

    style: improve style

--- lib/game.rb
+++ lib/game.rb
@@ -6,16 +6,12 @@ module Game
         real_score_team2 < 0 || real_score_team2 > 9 ||
         estimated_score_team1 < 0 || estimated_score_team1 > 9 ||
         estimated_score_team2 < 0 || estimated_score_team2 > 9
-      raise ArgumentError.new("unexpected score of the game of team other than 0-9")
-    elsif real_score_team1 == estimated_score_team1 &&
-        real_score_team2 == estimated_score_team2
+      raise ArgumentError, "Unexpected score of the game of team other than 0-9"
+    elsif real_score_team1 == estimated_score_team1 && real_score_team2 == estimated_score_team2
       1
-    elsif real_score_team1 > real_score_team2 &&
-        estimated_score_team1 > estimated_score_team2 ||
-        real_score_team1 < real_score_team2 &&
-            estimated_score_team1 < estimated_score_team2 ||
-        real_score_team1 == real_score_team2 &&
-            estimated_score_team1 == estimated_score_team2
+    elsif real_score_team1 > real_score_team2 && estimated_score_team1 > estimated_score_team2 ||
+        real_score_team1 < real_score_team2 && estimated_score_team1 < estimated_score_team2 ||
+        real_score_team1 == real_score_team2 && estimated_score_team1 == estimated_score_team2
       0
     else
       -1
