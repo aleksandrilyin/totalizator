b0828eb88e3ad9ae5e0b4c9e7daaa0bcae4c56cb
Alexander Ilyin <alexander.ilyin11@gmail.com>
2021-12-16 11:51:21 +0300

    refactor: use spaceship operator

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,9 @@
 
 module Game
   def score(real_score_team1, real_score_team2, estimated_score_team1, estimated_score_team2)
+    real_score_comparison = real_score_team1 <=> real_score_team2
+    estimated_score_comparison = estimated_score_team1 <=> estimated_score_team2
+
     if real_score_team1 < 0 || real_score_team1 > 9 ||
         real_score_team2 < 0 || real_score_team2 > 9 ||
         estimated_score_team1 < 0 || estimated_score_team1 > 9 ||
@@ -9,9 +12,9 @@ module Game
       raise ArgumentError, "Unexpected score of the game of team other than 0-9"
     elsif real_score_team1 == estimated_score_team1 && real_score_team2 == estimated_score_team2
       1
-    elsif real_score_team1 > real_score_team2 && estimated_score_team1 > estimated_score_team2 ||
-        real_score_team1 < real_score_team2 && estimated_score_team1 < estimated_score_team2 ||
-        real_score_team1 == real_score_team2 && estimated_score_team1 == estimated_score_team2
+    elsif real_score_comparison.positive? && estimated_score_comparison.positive? ||
+        real_score_comparison.negative? && estimated_score_comparison.negative? ||
+        real_score_comparison.zero? && estimated_score_comparison.zero?
       0
     else
       -1
